7370b776be84fb0c35d35df591aeae002925a25b
Cyanism <61608977+Cyanistic@users.noreply.github.com>
2023-05-13 20:30:37 -0400

    formatted code

--- src/lib.rs
+++ src/lib.rs
@@ -1,92 +1,131 @@
-use std::{io::Write, time::Duration};
-use std::path::Path;
-use std::fs::File;
-use std::thread::sleep;
-use mongodb::bson::{Document, doc};
-use mongodb::{Client as MongoClient, options::FindOptions, Cursor}; 
-use json::*;
-use mpd::{Client as MPDClient};
 use futures::stream::{StreamExt, TryStreamExt};
+use json::*;
+use mongodb::bson::{doc, Document};
+use mongodb::{options::FindOptions, Client as MongoClient, Cursor};
+use mpd::Client as MPDClient;
+use std::fs::File;
+use std::path::Path;
+use std::thread::sleep;
+use std::{io::Write, time::Duration};
 
-pub fn create_config_dir(){
-    match std::fs::create_dir_all(Path::new(&(dirs::config_dir().unwrap().display().to_string() + "/mpdtrackr"))){
+pub fn create_config_dir() {
+    match std::fs::create_dir_all(Path::new(
+        &(dirs::config_dir().unwrap().display().to_string() + "/mpdtrackr"),
+    )) {
         Ok(k) => k,
-        Err(e) => println!("Could not create config directory: {}", e)
+        Err(e) => println!("Could not create config directory: {}", e),
     };
-
 }
 
-pub fn create_config(){
+pub fn create_config() {
     let dir = dirs::config_dir().unwrap().display().to_string() + "/mpdtrackr";
-    let config_file_dir = dirs::config_dir().unwrap().display().to_string() + "/mpdtrackr/config.json";
+    let config_file_dir =
+        dirs::config_dir().unwrap().display().to_string() + "/mpdtrackr/config.json";
     let config_dir = Path::new(&dir);
     let config_file_path = Path::new(&config_file_dir);
-    match config_dir.is_dir(){
+    match config_dir.is_dir() {
         false => create_config_dir(),
-        true => ()
+        true => (),
     };
-    let mut config = match config_file_path.is_file(){
+    let mut config = match config_file_path.is_file() {
         false => File::create(config_file_path).unwrap(),
-        true => File::options().read(true).write(true).open(config_file_path).unwrap()
+        true => File::options()
+            .read(true)
+            .write(true)
+            .open(config_file_path)
+            .unwrap(),
     };
-    match config.write_all("{\n \"mongo_port\": 27017,\n \"mpd_port\": 6600\n}".as_bytes()){
+    match config.write_all("{\n \"mongo_port\": 27017,\n \"mpd_port\": 6600\n}".as_bytes()) {
         Ok(_) => (),
-        Err(e) => println!("{}", e)
+        Err(e) => println!("{}", e),
     };
 }
 
-pub fn import(mongo_client: MongoClient, files: Vec<String>){
+pub fn import(mongo_client: MongoClient, files: Vec<String>) {}
 
-}
+pub fn output(mongo_client: MongoClient, files: Vec<String>) {}
 
-pub fn output(mongo_client: MongoClient, files: Vec<String>){
-
-}
-
-pub async fn print(mongo_client: MongoClient){
+pub async fn print(mongo_client: MongoClient) {
     let db = mongo_client.database("mpdtrackr");
-    let find_options = FindOptions::builder().sort(doc!{"time": -1}).projection(doc!{"_id": 0}).build();
+    let find_options = FindOptions::builder()
+        .sort(doc! {"time": -1})
+        .projection(doc! {"_id": 0})
+        .build();
     for collection_name in db.list_collection_names(None).await.unwrap() {
-        let mut cursor = db.collection::<Document>(collection_name.as_str()).find(None, find_options.clone()).await.unwrap();
-        while let Some(item) = cursor.try_next().await.unwrap(){
+        let mut cursor = db
+            .collection::<Document>(collection_name.as_str())
+            .find(None, find_options.clone())
+            .await
+            .unwrap();
+        while let Some(item) = cursor.try_next().await.unwrap() {
             println!("{}", item);
         }
     }
 }
 
-pub async fn run(mongo_client: MongoClient, mut mpd_client: MPDClient, config: JsonValue){
+pub async fn run(mongo_client: MongoClient, mut mpd_client: MPDClient, config: JsonValue) {
     let db = mongo_client.database("mpdtrackr");
     let mongo_artists = db.collection::<Document>("artists");
     let mongo_songs = db.collection::<Document>("songs");
     loop {
-        let (mut current_time, duration) = (mpd_client.status().unwrap().time.unwrap().0.num_seconds(), mpd_client.status().unwrap().time.unwrap().1.num_seconds());
+        let (mut current_time, duration) = (
+            mpd_client.status().unwrap().time.unwrap().0.num_seconds(),
+            mpd_client.status().unwrap().time.unwrap().1.num_seconds(),
+        );
         let song = mpd_client.currentsong().unwrap().unwrap();
         let artist = song.tags.get("Artist").unwrap();
         let title = song.title.clone().unwrap().clone();
-        match mongo_artists.find_one(doc!{"artist": artist}, None).await.unwrap(){
+        match mongo_artists
+            .find_one(doc! {"artist": artist}, None)
+            .await
+            .unwrap()
+        {
             None => {
-                mongo_artists.insert_one(doc!{"artist": artist, "time": 0}, None).await.unwrap();
+                mongo_artists
+                    .insert_one(doc! {"artist": artist, "time": 0}, None)
+                    .await
+                    .unwrap();
                 ()
-            },
-            _ => ()
+            }
+            _ => (),
         }
-        match mongo_songs.find_one(doc!{"title": &title}, None).await.unwrap(){
+        match mongo_songs
+            .find_one(doc! {"title": &title}, None)
+            .await
+            .unwrap()
+        {
             None => {
-                mongo_songs.insert_one(doc!{"title": &title, "artist": artist, "time": 0}, None).await.unwrap();
+                mongo_songs
+                    .insert_one(doc! {"title": &title, "artist": artist, "time": 0}, None)
+                    .await
+                    .unwrap();
                 ()
-            },
-            _ => ()
+            }
+            _ => (),
         }
         let mut old_time = current_time;
-        while title == mpd_client.currentsong().unwrap().unwrap().title.clone().unwrap(){
+        while title
+            == mpd_client
+                .currentsong()
+                .unwrap()
+                .unwrap()
+                .title
+                .clone()
+                .unwrap()
+        {
             current_time = mpd_client.status().unwrap().time.unwrap().0.num_seconds();
             sleep(Duration::from_millis(999));
-            if old_time + 1 <= current_time{
-                mongo_artists.update_one(doc!{"artist": artist}, doc!{"$inc": {"time": 1}} , None).await.unwrap();
-                mongo_songs.update_one(doc!{"title": &title}, doc!{"$inc": {"time": 1}} , None).await.unwrap();
+            if old_time + 1 <= current_time {
+                mongo_artists
+                    .update_one(doc! {"artist": artist}, doc! {"$inc": {"time": 1}}, None)
+                    .await
+                    .unwrap();
+                mongo_songs
+                    .update_one(doc! {"title": &title}, doc! {"$inc": {"time": 1}}, None)
+                    .await
+                    .unwrap();
                 old_time = current_time;
-            }
-            else if old_time > current_time{
+            } else if old_time > current_time {
                 old_time = current_time;
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use mongodb::{Client as MongoClient, options::ClientOptions};
-use clap::{Parser};
+use clap::Parser;
 use json::*;
-use mpd::{Client as MPDClient};
+use mongodb::{options::ClientOptions, Client as MongoClient};
+use mpd::Client as MPDClient;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
@@ -10,15 +10,15 @@ mod lib;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about)]
 /// Program that tracks mpd listening time on a per artist and per song basis
-pub struct Args{
+pub struct Args {
     /// Import database from files
     #[arg(short, long)]
     import: Option<Vec<String>>,
-    
+
     /// Print the database to stdout
-    #[arg(short, long, default_value_t=false)]
+    #[arg(short, long, default_value_t = false)]
     print: bool,
-    
+
     /// Files to output the database to
     #[arg(short, long)]
     output: Option<Vec<String>>,
@@ -27,9 +27,10 @@ pub struct Args{
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
-    let config_file_dir_literal = dirs::config_dir().unwrap().to_str().unwrap().to_string() +"/mpdtrackr/config.json";
+    let config_file_dir_literal =
+        dirs::config_dir().unwrap().to_str().unwrap().to_string() + "/mpdtrackr/config.json";
     let config_file_dir = Path::new(&config_file_dir_literal);
-    let mut config_file = match File::open(&config_file_dir){
+    let mut config_file = match File::open(&config_file_dir) {
         Ok(k) => k,
         Err(_) => {
             lib::create_config();
@@ -37,12 +38,14 @@ async fn main() {
         }
     };
     let mut config_file_contents = String::new();
-    config_file.read_to_string(&mut config_file_contents).unwrap();
-    let config = match parse(config_file_contents.as_str()){
+    config_file
+        .read_to_string(&mut config_file_contents)
+        .unwrap();
+    let config = match parse(config_file_contents.as_str()) {
         Ok(k) => k,
         Err(_) => {
             lib::create_config();
-            match parse(config_file_contents.as_str()){
+            match parse(config_file_contents.as_str()) {
                 Ok(k) => k,
                 Err(e) => {
                     println!("{}", e);
@@ -52,13 +55,30 @@ async fn main() {
         }
     };
 
-    let mongo_client_options = ClientOptions::parse(format!("{}{}", "mongodb://localhost:", config["mongo_port"])).await.unwrap();
+    let mongo_client_options = ClientOptions::parse(format!(
+        "{}{}",
+        "mongodb://localhost:", config["mongo_port"]
+    ))
+    .await
+    .unwrap();
     let mongo_client = MongoClient::with_options(mongo_client_options).unwrap();
     let mpd_client = MPDClient::connect(format!("{}{}", "localhost:", config["mpd_port"])).unwrap();
     match args {
-        Args{ import: Some(files), output: _, print: _ } => lib::import(mongo_client, files),
-        Args{ import: _, output: Some(files), print: _ } => lib::output(mongo_client, files),
-        Args{ import: _, output: _, print: true } => lib::print(mongo_client).await,
-        _ => lib::run(mongo_client, mpd_client, config).await
+        Args {
+            import: Some(files),
+            output: _,
+            print: _,
+        } => lib::import(mongo_client, files),
+        Args {
+            import: _,
+            output: Some(files),
+            print: _,
+        } => lib::output(mongo_client, files),
+        Args {
+            import: _,
+            output: _,
+            print: true,
+        } => lib::print(mongo_client).await,
+        _ => lib::run(mongo_client, mpd_client, config).await,
     }
 }
