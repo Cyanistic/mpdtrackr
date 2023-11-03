2fc05d669b0b571740054361e24a9e1fd3a53f35
Cyanism <61608977+Cyanistic@users.noreply.github.com>
2023-11-03 16:50:35 -0400

    added file locks and basic printing
    - added basic printing to stdout
    - added file locks to only allow one instance running at a time to prevent duplicate writes to the db

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@ use std::{
     cmp::Ordering,
     collections::HashMap,
     fs::{create_dir_all, File},
-    io::{Read, Write},
+    path::PathBuf,
+    sync::Mutex,
     time::Duration,
 };
 
 use anyhow::{anyhow, Result};
 use clap::{Parser, Subcommand};
+use fs2::FileExt;
+use futures::StreamExt;
 use mpd::{Client, State};
 use tokio::time::Instant;
 
@@ -38,6 +41,10 @@ enum SubCommand {
         months: Option<usize>,
         #[arg(short, long)]
         years: Option<usize>,
+        #[arg(short, long)]
+        verbose: bool,
+        #[arg(short, long)]
+        json: bool,
     },
 }
 
@@ -68,7 +75,7 @@ CREATE TABLE IF NOT EXISTS songs (
  id INTEGER PRIMARY KEY AUTOINCREMENT,
  title TEXT NOT NULL,
  artist_id INTEGER,
- album, TEXT
+ album TEXT,
  genre TEXT,
  FOREIGN KEY (artist_id)
     REFERENCES artists (id) 
@@ -99,7 +106,30 @@ CREATE TABLE IF NOT EXISTS listening_times (
             weeks,
             months,
             years,
-        } => print().await,
+            verbose,
+            json,
+        } => {
+            print(
+                &pool,
+                [days, weeks, months, years]
+                    .into_iter()
+                    .enumerate()
+                    .map(|(k, v)| {
+                        v.unwrap_or(0)
+                            * match k {
+                                0 => 1,
+                                1 => 7,
+                                2 => 30,
+                                3 => 365,
+                                _ => 0,
+                            }
+                    })
+                    .sum(),
+                verbose,
+                json,
+            )
+            .await?
+        }
         SubCommand::Export { files } => export(files).await,
         SubCommand::Import { files } => import(files).await,
     }
@@ -107,33 +137,52 @@ CREATE TABLE IF NOT EXISTS listening_times (
 }
 
 async fn run(pool: &sqlx::SqlitePool) -> Result<()> {
+    let lock_file =
+        File::create(std::env::temp_dir().join(concat!(env!("CARGO_PKG_NAME"), ".lock")))?;
+    lock_file.try_lock_exclusive().expect(concat!(
+        "An instance of ",
+        env!("CARGO_PKG_NAME"),
+        " is already running!"
+    ));
     let mut mpd = Client::connect(format!("127.0.0.1:{}", 6600))?;
     'outer: loop {
         let outer_song = mpd
             .currentsong()?
             .ok_or(anyhow!("Error: no song playing"))?;
         let tags = HashMap::<String, String>::from_iter(outer_song.tags.clone());
-        let artist_id =
-            match sqlx::query!("SELECT * FROM artists WHERE name = $1", outer_song.artist)
-                .fetch_optional(pool)
-                .await?
-            {
-                Some(k) => k.id,
-                None => {
-                    sqlx::query("INSERT INTO artists VALUES ($1, $2)")
-                        .bind::<Option<&str>>(None)
-                        .bind(outer_song.artist.as_ref().unwrap())
-                        .execute(pool)
-                        .await?;
-                    continue;
-                }
-            };
-        let song_id = match sqlx::query!(
-            "SELECT id, title FROM songs WHERE title = $1",
-            outer_song.title
-        )
-        .fetch_optional(pool)
-        .await?
+        let artist = match outer_song.artist {
+            Some(k) => Some(k),
+            None => {
+                let path = PathBuf::from(&outer_song.file);
+                path.file_stem()
+                    .and_then(|x| x.to_str())
+                    .and_then(|x| x.find('-').map(|ind| x[..ind].trim().to_string()))
+            }
+        };
+        let artist_id = match sqlx::query!("SELECT * FROM artists WHERE name = $1", artist)
+            .fetch_optional(pool)
+            .await?
+        {
+            Some(k) => k.id,
+            None => {
+                sqlx::query!("INSERT INTO artists VALUES ($1, $2)", None::<u8>, artist)
+                    .execute(pool)
+                    .await?;
+                continue;
+            }
+        };
+        let title = match outer_song.title.clone() {
+            Some(k) => Some(k),
+            None => {
+                let path = PathBuf::from(&outer_song.file);
+                path.file_stem()
+                    .and_then(|x| x.to_str())
+                    .and_then(|x| x.find('-').map(|ind| x[ind + 1..].trim().to_string()))
+            }
+        };
+        let song_id = match sqlx::query!("SELECT id, title FROM songs WHERE title = $1", title)
+            .fetch_optional(pool)
+            .await?
         {
             Some(k) => k.id,
             None => {
@@ -142,7 +191,7 @@ async fn run(pool: &sqlx::SqlitePool) -> Result<()> {
                 sqlx::query!(
                     "INSERT INTO songs VALUES ($1, $2, $3, $4, $5)",
                     None::<u8>,
-                    outer_song.title,
+                    title,
                     artist_id,
                     album,
                     genre
@@ -152,10 +201,10 @@ async fn run(pool: &sqlx::SqlitePool) -> Result<()> {
                 continue;
             }
         };
-        let now = chrono::Local::now().date_naive();
+        let date = chrono::Local::now().date_naive();
         if sqlx::query!(
             "SELECT * from listening_times where date = $1 and song_id = $2",
-            now,
+            date,
             song_id
         )
         .fetch_optional(pool)
@@ -165,7 +214,7 @@ async fn run(pool: &sqlx::SqlitePool) -> Result<()> {
             sqlx::query!(
                 "INSERT INTO listening_times VALUES ($1, $2, $3, $4)",
                 None::<u8>,
-                now,
+                date,
                 song_id,
                 0
             )
@@ -188,7 +237,7 @@ async fn run(pool: &sqlx::SqlitePool) -> Result<()> {
                         Some(k) => k,
                         None => continue 'inner,
                     };
-                    if outer_song == inner_song {
+                    if outer_song.title == inner_song.title {
                         current_time = mpd
                             .status()?
                             .time
@@ -196,13 +245,18 @@ async fn run(pool: &sqlx::SqlitePool) -> Result<()> {
                             .0;
                         match old_time.cmp(&current_time) {
                             Ordering::Less => {
-                                sqlx::query!("UPDATE listening_times SET playback_time = playback_time + 1 WHERE song_id = $1", song_id).execute(pool).await?;
+                                sqlx::query!("UPDATE listening_times SET playback_time = playback_time + 1 WHERE song_id = $1 AND date = $2", song_id, date).execute(pool).await?;
                                 old_time = current_time
                             }
                             Ordering::Greater => old_time = current_time,
                             Ordering::Equal => (),
                         }
-                        tokio::time::sleep(Duration::from_secs(1) - now.elapsed()).await
+                        tokio::time::sleep(
+                            Duration::from_secs(1)
+                                .checked_sub(now.elapsed())
+                                .unwrap_or_default(),
+                        )
+                        .await
                     } else {
                         continue 'outer;
                     }
@@ -212,8 +266,36 @@ async fn run(pool: &sqlx::SqlitePool) -> Result<()> {
     }
 }
 
-async fn print() {
-    todo!()
+async fn print(pool: &sqlx::SqlitePool, days: usize, verbose: bool, json: bool) -> Result<()> {
+    match verbose {
+        true => {
+            let mut query = sqlx::query!("SELECT songs.*, artists.name AS artist, SUM(listening_times.playback_time) AS playback_time FROM songs INNER JOIN listening_times ON songs.id = listening_times.song_id INNER JOIN artists ON artists.id = songs.artist_id GROUP BY songs.id;").fetch(pool);
+            while let Some(entry) = query.next().await {
+                let entry = entry?;
+                let playback_time = format!(
+                    "{}h{}m{}s",
+                    &entry.playback_time / 3600,
+                    (&entry.playback_time % 3600) / 60,
+                    (&entry.playback_time % 60)
+                );
+                println!("{:?} {}", entry, playback_time);
+            }
+        }
+        false => {
+            let mut query = sqlx::query!("SELECT songs.title, artists.name AS artist, SUM(listening_times.playback_time) AS playback_time FROM songs INNER JOIN listening_times ON songs.id = listening_times.song_id INNER JOIN artists ON artists.id = songs.artist_id GROUP BY songs.id;").fetch(pool);
+            while let Some(entry) = query.next().await {
+                let entry = entry?;
+                let playback_time = format!(
+                    "{}h{}m{}s",
+                    &entry.playback_time / 3600,
+                    (&entry.playback_time % 3600) / 60,
+                    (&entry.playback_time % 60)
+                );
+                println!("{:?} {}", entry, playback_time);
+            }
+        }
+    }
+    Ok(())
 }
 
 async fn import(files: Vec<String>) {
